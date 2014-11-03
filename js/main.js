85cad99462e2d778bc5e95f5ed19ad28687bdb8c
Margarete Koenen <margarete.koenen@loudfeed.com>
2014-11-02 20:46:10 -0500

    Part 6
    adding home view.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 var app = {
 
-    renderHomeView: function() {
-        var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                    "<input class='search-key'/>" +
-                    "<ul class='employee-list'></ul>" +
-                "</div>"
-        $('body').html(html);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -35,11 +11,9 @@ var app = {
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.renderHomeView();
-    });
+            $('body').html(new HomeView(self.store).render().el);
+        });
     }
-
-
 
 };
 
